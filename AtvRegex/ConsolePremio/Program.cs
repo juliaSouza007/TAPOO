4311deb80249e1a977bf51fde5716672658049fe
Júlia Martins Souza <juliamss07@gmail.com>
2025-06-18 17:52:13 -0300

    finish regex!!

---
+++ AtvRegex/ConsolePremio/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.IO;
+using System.Text.Json;
+using System.Text.Json.Nodes;
+
+class Program
+{
+    static void Main()
+    {
+        string json = File.ReadAllText("prize.json"); 
+        JsonNode root = JsonNode.Parse(json);
+
+        foreach (JsonNode prize in root["prizes"].AsArray())
+        {
+            if (prize["category"]?.ToString() == "economics")
+            {
+                var laureates = prize["laureates"].AsArray();
+                if (laureates.Count > 0)
+                {
+                    string firstname = laureates[0]["firstname"]?.ToString();
+
+                    Console.WriteLine("-------------------------");
+                    Console.WriteLine("Lendo o arquivo JSON...");
+                    Console.WriteLine("-------------------------\n");
+
+                    Console.WriteLine($"Categoria: {prize["category"]}");
+                    Console.ForegroundColor = ConsoleColor.Green;
+                    Console.WriteLine($"Primeiro ganhador de economia: {firstname}");
+                    Console.ResetColor();
+                    break;
+                }
+            }
+        }
+    }
+}
---
+++ AtvRegex/ConsoleSenha/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+class Program
+{
+    static void Main()
+    {
+        string pattern = @"^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[!@#$%^&*()+=_\-{}\[\]:;""'?<>,.]).{7,16}$";
+
+        while (true)
+        {
+            Console.Write("Digite uma senha: ");
+            string senha = Console.ReadLine();
+
+            bool temMinuscula = Regex.IsMatch(senha, "[a-z]");
+            bool temMaiuscula = Regex.IsMatch(senha, "[A-Z]");
+            bool temNumero = Regex.IsMatch(senha, @"\d");
+            bool temEspecial = Regex.IsMatch(senha, @"[!@#$%^&*()+=_\-{}\[\]:;""'?<>,.]");
+            bool tamanhoCorreto = senha.Length >= 7 && senha.Length <= 16;
+
+            Console.WriteLine("\nVerificando requisitos:\n");
+
+            MostrarRequisito("Entre 7 e 16 caracteres", tamanhoCorreto);
+            MostrarRequisito("Pelo menos uma letra minúscula", temMinuscula);
+            MostrarRequisito("Pelo menos uma letra maiúscula", temMaiuscula);
+            MostrarRequisito("Pelo menos um número", temNumero);
+            MostrarRequisito("Pelo menos um caractere especial permitido", temEspecial);
+
+            if (Regex.IsMatch(senha, pattern))
+            {
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine("\n✔️ Senha forte! Programa finalizado.");
+                Console.ResetColor();
+                break;
+            }
+            else
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("\n❌ Senha fraca! Tente novamente.\n");
+                Console.ResetColor();
+            }
+        }
+    }
+
+    static void MostrarRequisito(string texto, bool atende)
+    {
+        Console.ForegroundColor = atende ? ConsoleColor.Green : ConsoleColor.Red;
+        Console.WriteLine($"{texto}");
+        Console.ResetColor();
+    }
+}
